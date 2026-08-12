af7ad7bebe362821f7556ab0af056d733e000710
Snehasish Das <dsnehasish73@gmail.com>
2026-08-12 21:23:20 +0530

    Fix pkg_resources for Razorpay

--- backend/config.py
+++ backend/config.py
@@ -1,26 +1,32 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from functools import lru_cache
-from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+
     mongodb_url: str
     jwt_secret: str
+
     jwt_algorithm: str = "HS256"
     access_token_expire_days: int = 7
+
     razorpay_key_id: str
     razorpay_key_secret: str
+
     app_name: str = "SoleStyle"
-    frontend_url: str = "http://127.0.0.1:5500"
+
+    frontend_url: str = "https://solestylee.netlify.app/"
 
     model_config = SettingsConfigDict(
-        env_file=Path(__file__).resolve().parent/".env",
+        env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore"
     )
 
+
 @lru_cache()
 def get_settings():
     return Settings()
+
 
 settings = get_settings()
